c51735710a9107af3b57a82e6a003e69388eccb7
Vinícius Gabriel <comovcfaraologin2018@gmail.com>
2023-07-09 00:23:20 -0300

    Subindo meu portfólio
    Subindo portfólio. Ainda não está finalizado.

---
+++ app.js
@@ -0,0 +1,35 @@
+var texts = [
+    "Desenvolvedor Front-And",
+    "Youtuber"
+];
+
+var textIndex = 0;
+var charIndex = 0;
+var textElement = document.getElementById("text-animation");
+
+function type(){
+    if( charIndex < texts[textIndex].length){
+        textElement.innerHTML += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 50);
+    } else{
+        setTimeout(erase, 2000);
+    }
+};
+
+function erase(){
+    if(charIndex > 0){
+        textElement.innerHTML = texts[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase , 50)
+    } else{
+        textIndex++;
+        if(textIndex >= texts.length){
+            textIndex = 0;
+        }
+
+        setTimeout(type, 500);
+    }
+}
+    
+setTimeout(type, 500);
